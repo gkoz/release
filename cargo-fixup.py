5c5780658b16624a2047926ab6c2e8462e3ec695
Gleb Kozyrev <gleb@gkoz.com>
2016-03-13 21:46:20 +0200

    Fixup `git = "..."` deps

--- cargo-fixup.py
+++ cargo-fixup.py
@@ -13,8 +13,9 @@ def manifests():
 
 crates = {}
 re_group = re.compile('^\s*\[(.*)\]\s*$')
+re_dependency = re.compile('^(?:build-|dev-)?dependencies\.(.*)$')
 re_name = re.compile('^\s*name\s*=\s*"(.*)"\s*$')
-re_crate = re.compile('^\s*([a-z0-9_-]+)\s*=\s*"(.*)"\s*$')
+re_git = re.compile('^\s*git\s*=.*$')
 
 for path in manifests():
 	manifest = open(os.path.join(path, CARGO_TOML), 'r')
@@ -34,23 +35,23 @@ for path in crates.values():
 	manifest = open(os.path.join(path, CARGO_TOML), 'r')
 	with open(os.path.join(path, CARGO_TOML_NEW), 'w') as new_manifest:
 			group = None
+			crate = None
 			for line in manifest:
 				match = re_group.match(line)
 				if match:
 					group = match.group(1)
-				if group == 'dependencies':
-					match = re_crate.match(line)
-					if match and match.group(1) in crates:
-						name = match.group(1)
-						ver = match.group(2)
-						rel_path = os.path.relpath(crates[name], path) \
+					match = re_dependency.match(group)
+					if match:
+							crate = match.group(1)
+					else:
+							crate = None
+				if crate in crates:
+					match = re_git.match(line)
+					if match:
+						rel_path = os.path.relpath(crates[crate], path) \
 										.replace(os.path.sep, '/')
 						new_manifest.write(
-							'%s = { version = "%s", path = "%s" }\n' % (
-								name,
-								ver,
-								rel_path
-							))
+							'path = "%s"\n' % rel_path)
 						continue
 				new_manifest.write(line)
 	manifest.close()
